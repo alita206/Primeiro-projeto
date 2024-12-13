be2885ef0812d2efc9c9bfc51070310402472e14
alita206 <139910522+alita206@users.noreply.github.com>
2024-12-13 11:49:48 -0300

    Sistema de pontos

--- Primeiro Projeto/Assets/Script/GameController.cs
+++ Primeiro Projeto/Assets/Script/GameController.cs
@@ -5,12 +5,18 @@ using System.Collections.Generic;
 
 public class GameController : MonoBehaviour
 {
+    [SerializeField] private float timeMin = 1f;
+    [SerializeField] private float timeMax = 3f;
     [SerializeField] private float timer = 1f;
     [SerializeField] private GameObject obstaculo;
     [SerializeField] private Vector3 posicao;
     [SerializeField] private float posMin = -3.58f;
     [SerializeField] private float posMax = 1.3f;
     [SerializeField] private float meuX = 12.64f;
+
+    //Variavel dos pontos 
+    private float pontos = 0f;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,12 +25,17 @@ public class GameController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    { //aumentar os pontos
+
+        pontos += Time.deltaTime;
+        Debug.Log(pontos);
+
         timer -= Time.deltaTime;
         if (timer <= 0 )
         {
             Debug.Log("criando");
-            timer = 1f;
+
+            timer = Random.Range(timeMax, timeMin);
 
             posicao.x = meuX;
             posicao.y = Random.Range( posMin, posMax );
